2bf44c7b49416af8294e36e26a57dc0df78409d2
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-09-12 11:17:01 +0530

    Update asgi.py

--- Text Utility/asgi.py
+++ Text Utility/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secondproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textutility.settings')
 
 application = get_asgi_application()
